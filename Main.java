4fa4d796695ab06096b6e303023d049de472c877
Adi <adi.tom.mail@gmail.com>
2024-10-17 22:04:45 +0200

    JFrame
    created Main and MyFrame
    added frame etc

---
+++ Main.java
@@ -0,0 +1,10 @@
+package cbl;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		MyFrame TicTacToe = new MyFrame();
+	
+	}
+}
---
+++ MyFrame.java
@@ -0,0 +1,39 @@
+package cbl;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+
+	MyFrame(){
+		
+		// Sets the frame's title
+		this.setTitle("Super Ultimate Tic-Tac-Toe");
+		
+		// Closes the application
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+        // Gets the screen size
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+
+        // Sets the frame size to the full screen size
+        this.setSize(screenSize.width, screenSize.height);
+
+        // Sets the frame to be maximized
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		// Makes the frame visible
+		this.setVisible(true);
+		
+		// Sets the background color
+		this.getContentPane().setBackground(new Color(157, 220, 242));
+		
+		// Changes the application's icon
+		ImageIcon image = new ImageIcon("23926137_l.jpg");
+		this.setIconImage(image.getImage());
+		
+	}
+	
+}
