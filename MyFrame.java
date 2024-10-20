02fe15a94f9983380eeceacd6c385e080d39a8f2
Adi <adi.tom.mail@gmail.com>
2024-10-20 04:50:00 +0200

    added winner checks :3
    sowwy for the late upload :c

--- MyFrame.java
+++ MyFrame.java
@@ -38,8 +38,11 @@ public class MyFrame extends JFrame {
 
 		JButton[][][] buttonGrid = new JButton[9][3][3];
 		int[][][] gridArray = new int[9][3][3];
+		
+		// Creates an array to mark if there has been a winner in one of the grids
+		boolean[] gridCompleted = new boolean[9];
 
-		// Create 9 panels for each 3x3 grid
+		// Creates 9 panels for each 3x3 grid
 		for (int box = 0; box < 9; box++) {
 			JPanel groupPanel = new JPanel(new GridLayout(3, 3)); // 3x3 grid for buttons
 			groupPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK)); // Optional: border for each group
@@ -60,6 +63,7 @@ public class MyFrame extends JFrame {
 					int finalBox = box;
 					int finalRow = row;
 					int finalCol = col;
+					
 					button.addActionListener(e -> {
 
 						turn();
@@ -92,6 +96,9 @@ public class MyFrame extends JFrame {
 
 						// Disables the button
 						button.setEnabled(false);
+						
+						// Checks if any of the players won
+						checkWin(gridArray, gridCompleted);
 
 					});
 
@@ -125,6 +132,58 @@ public class MyFrame extends JFrame {
 			crossTurn = false;
 		}
 
+	}
+	
+	public static void checkWin(int[][][] gridArray, boolean[] gridCompleted) {
+			
+		for (int i = 0; i < 9; i++) {
+			
+			// If there is a winner in this grid, skip it
+			if (gridCompleted[i]) {
+				continue;
+			}
+			
+			for (int j = 0; j < 3; j++) {
+				
+					// Check horizontal
+				if (gridArray[i][j][0] == gridArray[i][j][1] && gridArray[i][j][1] == gridArray[i][j][2] && gridArray[i][j][0] != 0) {
+					announceWinner(gridArray[i][j][0], i, gridCompleted);
+					return;
+				}
+				
+				 // Check vertical
+	            if (gridArray[i][0][j] == gridArray[i][1][j] && gridArray[i][1][j] == gridArray[i][2][j] && gridArray[i][0][j] != 0) {
+	                announceWinner(gridArray[i][0][j], i, gridCompleted);
+	                return;
+	            }
+	            
+	            // Check diagonal
+	            if (gridArray[i][0][0] == gridArray[i][1][1] && gridArray[i][1][1] == gridArray[i][2][2] && gridArray[i][0][0] != 0) {
+	                announceWinner(gridArray[i][0][0], i, gridCompleted);
+	                return;
+	            }
+	            if (gridArray[i][0][2] == gridArray[i][1][1] && gridArray[i][1][1] == gridArray[i][2][0] && gridArray[i][0][2] != 0) {
+	                announceWinner(gridArray[i][0][2], i, gridCompleted);
+	                return;
+	            }
+			}
+		}
+	}
+	
+	public static void announceWinner(int player, int i, boolean[] gridCompleted) {
+		
+		if (player == 1) {
+			
+	        System.out.println("Cross wins!");
+	        
+	    } else if (player == 2) {
+	    	
+	        System.out.println("Circle wins!");
+	        
+	    }
+		
+		gridCompleted[i] = true;
+		
 	}
 
 	// Main method to launch the application
